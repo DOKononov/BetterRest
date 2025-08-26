33972cdb6a9b7e2c454ce2bb0812eb3603105358
d.o.kononov <d.o.kononov@gmail.com>
2025-08-26 11:42:54 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dmitry Kononov on 26.08.25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
